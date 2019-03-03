b60c23992883710e9b2cea9a3b34227d62ea24e4
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-03 16:03:09 +0700

    rescue_error

--- app/controllers/api/v1/try_uiza_controller.rb
+++ app/controllers/api/v1/try_uiza_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::TryUizaController < ApplicationController
   def execute
     code = params[:code]
-    @result = eval code
+
+    begin
+      @result = eval(code).to_s
+    rescue => error
+      @result = error.inspect
+    end
+
     render template: "api/v1/try_uiza/execute.json"
   end
 end
